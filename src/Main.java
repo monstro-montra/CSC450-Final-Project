ee55a995f63f1a00971ba5894254396c323f6bbe
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-01-12 01:54:26 -0500

    completed multithreading class and finished driver class, "main"

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        Multithreading forwardCount = new Multithreading(20, 'f', 250);
+        Multithreading reverseCount = new Multithreading (20, 'r', 250);
+        Thread thread1 = new Thread(forwardCount);
+        Thread thread2 = new Thread(reverseCount);
+
+        System.out.print("Thread 1: ");
+        thread1.start();
+        try{
+            in.nextLine();
+            thread1.join();
+        } catch (InterruptedException ignored){
+        }
+
+        System.out.print("\nThread 2: ");
+        thread2.start();
+        try{
+            in.nextLine();
+            thread2.join();
+        } catch (InterruptedException ignored){
+        }
+
+    }
 }
---
+++ src/Multithreading.java
@@ -0,0 +1,46 @@
+public class Multithreading implements Runnable{
+    private final int amt;
+    private final char whichWay;
+    private final int speed;
+
+    public Multithreading(int amt, char whichWay, int speed){
+        this.amt = amt;
+        this.whichWay = whichWay;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run(){
+        count(amt, whichWay, speed);
+    }
+
+    public void count(int amt, char whichWay, int speed) {
+        switch(whichWay){
+            case 'f' : //case for incrementing up
+                for (int i = 0; i <= amt; i++){ // loop as long as i <= amt, incrementing i by 1 each time
+                    System.out.print(i + " ");
+                    try{
+                        Thread.sleep(speed);
+                    } catch (InterruptedException er) {
+                        System.out.println("Error, please try again.");
+                    }
+                }
+                return;
+
+
+            case 'r': //case for decrementing down from amt
+                for (int i = amt; i >= 0; i--){ //loop as long as i >= 0, decrementing i by 1 each time
+                    System.out.print(i + " ");
+                    try{
+                        Thread.sleep(speed);
+                    } catch (InterruptedException er){
+                        System.out.println("Error, please try again.");
+                    }
+                }
+
+
+        }
+
+    }
+
+}
